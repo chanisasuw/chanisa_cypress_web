f0a3357ef3cb941d7bbd222acfe9c63abfc9049d
chanisasuw <chanisa.suw@truedigital.com>
2023-09-27 17:33:35 +0200

    update code test verify alert java pop-up

--- cypress/integration/examples/Chanisa-test-2.js
+++ cypress/integration/examples/Chanisa-test-2.js
@@ -2,46 +2,46 @@
 
 describe('Chanisa Demo some features cypress', () => {
 
-//        it('User select radio button', () => {
-//          cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
-//          cy.contains("Radio buttons")
-//
-//          cy.get('[type="radio"]')
-//            .check('female').should('be.checked')
-//        })
-//
-//        it('User select one checkbox button', () => {
-//          cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
-//          cy.contains("Checkboxes")
-//
-//          cy.get('[type="checkbox"]')
-//          .check('Bike').should('be.checked')
-//
-//          cy.get('[type="checkbox"]')
-//          .uncheck('Bike').should('not.be.checked')
-//
-//        })
-//
-//        it('User select multiple checkbox button', () => {
-//          cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
-//          cy.contains("Checkboxes")
-//
-//          cy.get('[type="checkbox"]')
-//          .check(['Bike', 'Car']).should('be.checked')
-//
-//          cy.get('[type="checkbox"]')
-//          .uncheck(['Bike', 'Car']).should('not.be.checked')
-//         })
-//
-//        it('User select an option', () => {
-//          cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
-//          cy.contains("Dropdown")
-//
-//          cy.get('select')
-//            .should('have.value', 'volvo')
-//
-//          cy.get('select').select('audi')
-//        })
+        it('User select radio button', () => {
+          cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
+          cy.contains("Radio buttons")
+
+          cy.get('[type="radio"]')
+            .check('female').should('be.checked')
+        })
+
+        it('User select one checkbox button', () => {
+          cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
+          cy.contains("Checkboxes")
+
+          cy.get('[type="checkbox"]')
+          .check('Bike').should('be.checked')
+
+          cy.get('[type="checkbox"]')
+          .uncheck('Bike').should('not.be.checked')
+
+        })
+
+        it('User select multiple checkbox button', () => {
+          cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
+          cy.contains("Checkboxes")
+
+          cy.get('[type="checkbox"]')
+          .check(['Bike', 'Car']).should('be.checked')
+
+          cy.get('[type="checkbox"]')
+          .uncheck(['Bike', 'Car']).should('not.be.checked')
+         })
+
+        it('User select an option', () => {
+          cy.visit('https://ultimateqa.com/simple-html-elements-for-automation/')
+          cy.contains("Dropdown")
+
+          cy.get('select')
+            .should('have.value', 'volvo')
+
+          cy.get('select').select('audi')
+        })
 
         it('User verify java alert pop-up', () => {
           cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
